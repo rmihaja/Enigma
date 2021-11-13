3e94feb735ff70065e23eba81441fa2d2783e623
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-11-13 18:28:34 +0100

    Create demande parametres.c

---
+++ sources/demande parametres.c
@@ -0,0 +1,14 @@
+struct matrice ∗
+nouvelle_matrice ( int n , int m ) {
+struct matrice ∗m ;
+m = ( struct matrice ∗) malloc ( sizeof ( struct matrice ) ) ;
+m−>n = n ;
+m−>m = m ;
+m−>t = ( float ∗∗) malloc ( n ∗sizeof ( float ∗) ) ;
+for ( int i = 0 ; i < n ; i++ ) {
+m−>t [ i ] = ( float ∗) malloc ( m ∗sizeof ( float ) ) ;
+}
+return m ;
+}
+
+
