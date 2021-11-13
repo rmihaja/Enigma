3f6b09633bc8fc68dec1a7905e13712ac63d2ddf
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-11-13 18:47:09 +0100

    Update demande parametres.c

--- sources/demande parametres.c
+++ sources/demande parametres.c
@@ -1,25 +1,40 @@
-struct matrice ∗
-nouvelle_matrice ( int n , int m ) {
-struct matrice ∗m ;
-m = ( struct matrice ∗) malloc ( sizeof ( struct matrice ) ) ;
-m−>n = n ;
-m−>m = m ;
-m−>t = ( float ∗∗) malloc ( n ∗sizeof ( float ∗) ) ;
-for ( int i = 0 ; i < n ; i++ ) {
-m−>t [ i ] = ( float ∗) malloc ( m ∗sizeof ( float ) ) ;
-}
-return m ;
-}
-
-printf("Quels sont les coefficients de la ligne %d ? \n",i+1);
-
-scanf("%f",&a);
-
 #include <stdio.h>
 #include <stdlib.h>
 
 struct parametres * demande_parametres(){
   struct parametres * param;
   param=(struct parametres *) malloc (sizeof (struct parametres));
-  scanf("%d
-  param->
+  printf("xi ? \n");
+  scanf("%f",&a);
+  param->xi=a;
+  
+  printf("yi ? \n");
+  scanf("%f",&b);
+  param->yi=b;
+  
+  printf("zi ? \n");
+  scanf("%f",&c);
+  param->zi=c;
+  
+  printf("dt ? \n");
+  scanf("%f",&d);
+  param->dt=d;
+  
+  printf("Tmax ? \n");
+  scanf("%f",&e);
+  param->Tmax=e;
+  
+  printf("σ ? \n");
+  scanf("%f",&f);
+  param->σ=f;
+  
+  printf("ρ ? \n");
+  scanf("%f",&g);
+  param->ρ=g;
+  
+  printf("β ? \n");
+  scanf("%f",&h);
+  param->β=h;
+  
+  return param;
+}
