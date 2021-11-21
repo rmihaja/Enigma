79949c7e243d6b543b6c5de7e298133ab31eea7b
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-11-21 13:13:22 +0100

    Update demande parametres.c

--- sources/demande parametres.c
+++ sources/demande parametres.c
@@ -6,36 +6,44 @@ struct parametres * demande_parametres(){
   struct parametres * param;
   param=(struct parametres *) malloc (sizeof (struct parametres));
   printf("xi ? \n");
+  float a;
   scanf("%f",&a);
   param->xi=a;
   
   printf("yi ? \n");
+  float b;
   scanf("%f",&b);
   param->yi=b;
   
   printf("zi ? \n");
+  float c;
   scanf("%f",&c);
   param->zi=c;
   
   printf("dt ? \n");
+  float d;
   scanf("%f",&d);
   param->dt=d;
   
   printf("Tmax ? \n");
+  float e;
   scanf("%f",&e);
   param->Tmax=e;
   
-  printf("σ ? \n");
+  printf("sigma ? \n");
+  float f;
   scanf("%f",&f);
-  param->σ=f;
+  param->sigma=f;
   
-  printf("ρ ? \n");
+  printf("Ro ? \n");
+  float g;
   scanf("%f",&g);
-  param->ρ=g;
+  param->Ro=g;
   
-  printf("β ? \n");
+  printf("Beta ? \n");
+  float h;
   scanf("%f",&h);
-  param->β=h;
+  param->Beta=h;
   
   return param;
-}
+};
