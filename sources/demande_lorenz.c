d72e863b5c84fffcc517ff93113513b050043246
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-12-09 21:20:58 +0100

    Update demande_lorenz.c

--- sources/demande_lorenz.c
+++ sources/demande_lorenz.c
@@ -6,13 +6,15 @@
 struct systeme_dynamique *demande_lorenz() {
     struct systeme_dynamique *systeme = (struct systeme_dynamique *)malloc(sizeof(struct systeme_dynamique));
     systeme->constantes = (struct constantes *) malloc(sizeof(struct constantes));
+    systeme->constantes->t= malloc(3*sizeof(float));
 
     printf("\nVeuillez entrer la valeur de σ : ");
-    scanf("%f", &systeme->constantes->sigma);
+    scanf("%f", &systeme->constantes->t[0]);
     printf("\nVeuillez entrer la valeur de ρ : ");
-    scanf("%f", &systeme->constantes->rho);
+    scanf("%f", &systeme->constantes->t[1]);
     printf("\nVeuillez entrer la valeur de β : ");
-    scanf("%f", &systeme->constantes->beta);
+    scanf("%f", &systeme->constantes->t[2]);
+    systeme->constantes->taille_tableau=3;
 
     systeme->equation = &lorenz_transform;
 
