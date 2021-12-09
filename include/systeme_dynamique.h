92aa2b59ccb94587eb35909d0c65a5300e7fa30e
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-12-09 20:09:40 +0100

    Update systeme_dynamique.h

--- include/systeme_dynamique.h
+++ include/systeme_dynamique.h
@@ -8,9 +8,13 @@ struct systeme_dynamique {
 // * génération (création) des systèmes dynamiques
 
 struct systeme_dynamique *demande_lorenz();
+struct systeme_dynamique *demande_hugo();
+struct systeme_dynamique *demande_mihaja();
 
 // * équations de la trajectoire des systèmes dynamiques du projet
 // les équations sont à normaliser avec une fin/début unique
 // TODO: définir si normalisation "_transform" OK
 
 struct position *lorenz_transform(struct position *p, struct constantes *cte);
+struct position *hugo_transform(struct position *p, struct constantes *cte);
+struct position *mihaja_transform(struct position *p, struct constantes *cte);
