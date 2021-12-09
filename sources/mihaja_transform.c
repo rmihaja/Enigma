aac858ae1a2ca8dfd56510ed52278adef53464cf
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-12-09 19:52:51 +0100

    Update mihaja_transform.c

--- sources/mihaja_transform.c
+++ sources/mihaja_transform.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include "parametres.h"
+#include "position.h"
+
+struct position *mihaja_transform(struct position *p, struct constantes *cte) {
+    return nouvelle_position(
+        p->x + cte->sigma * (p->x - p->y),
+        p->y + (p->z) * (cte->rho - p->y) - p->x,
+        p->z + (p->y) - (cte->beta * p->x * p->z)
+    );
+}
+
+#ifdef TESTS
+
+int main() {
+    // tests here
+    return 0;
+}
+
+#endif
 
