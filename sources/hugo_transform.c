31620371290755103adbe9c4117a87f9c535f7ea
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-12-09 21:30:23 +0100

    Update hugo_transform.c

--- sources/hugo_transform.c
+++ sources/hugo_transform.c
@@ -4,9 +4,9 @@
 
 struct position *hugo_transform(struct position *p, struct constantes *cte) {
     return nouvelle_position(
-        p->x + cte->sigma * (p->y + p->x),
-        p->y + (cte->rho) * (p->x - p->z) - p->z,
-        p->z + (p->x) - (cte->beta * p->z)
+        p->x + cte->t[0] * (p->y + p->x),
+        p->y + (cte->t[1]) * (p->x - p->z) - p->z,
+        p->z + (p->x) - (p->z)
     );
 }
 
