45f16cbc5c6e953e579b5167a2e98456100356a4
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-12-09 19:47:05 +0100

    Create demande_hugo.c

---
+++ sources/demande_hugo.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "parametres.h"
+#include "systeme_dynamique.h"
+
+struct systeme_dynamique *demande_Hugo() {
+    struct systeme_dynamique *systeme = (struct systeme_dynamique *)malloc(sizeof(struct systeme_dynamique));
+    systeme->constantes = (struct constantes *) malloc(sizeof(struct constantes));
+
+    printf("\nVeuillez entrer la valeur de σ : ");
+    scanf("%f", &systeme->constantes->sigma);
+    printf("\nVeuillez entrer la valeur de ρ : ");
+    scanf("%f", &systeme->constantes->rho);
+    printf("\nVeuillez entrer la valeur de β : ");
+    scanf("%f", &systeme->constantes->beta);
+
+    systeme->equation = &hugo_transform;
+
+    return systeme;
+}
+
+#ifdef TESTS
+
+int main() {
+    // tests here
+    return 0;
+}
+
+#endif
