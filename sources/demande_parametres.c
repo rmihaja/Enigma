faf261351f8a0a265b56b619ff0fb91a133e7e15
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-12-09 20:04:12 +0100

    Update demande_parametres.c

--- sources/demande_parametres.c
+++ sources/demande_parametres.c
@@ -1,1 +1,36 @@
-trajectoire->parametres->nbre_points = (int) (trajectoire->parametres->t_max / trajectoire->parametres->dt);
+#include <stdio.h>
+#include <stdlib.h>
+#include "position.h"
+#include "parametres.h"
+
+struct parametres *demande_parametres() {
+  struct parametres *param;
+  param = (struct parametres *) malloc(sizeof(struct parametres));
+
+  param->position_initiale = (struct position *) malloc(sizeof(struct position));
+  printf("Veuillez entrer la position initial du système:\n");
+  printf("\nCoordonnée x : ");
+  scanf("%f", &param->position_initiale->x);
+  printf("\nCoordonnée y : ");
+  scanf("%f", &param->position_initiale->y);
+  printf("\nCoordonnée z : ");
+  scanf("%f", &param->position_initiale->z);
+  
+  printf("\nVeuillez entrer l'incrément dt du mouvement: ");
+  scanf("%f", &param->dt);
+  printf("\nVeuillez entrer le temps d'arrêt du système: ");
+  scanf("%f", &param->t_max);
+  
+  param->nbre_points = (int) (param->t_max / param->dt);
+    
+  return param;
+}
+
+#ifdef TESTS
+
+int main() {
+    // tests here
+    return 0;
+}
+
+#endif
