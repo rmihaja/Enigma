986fc49ca8ba0a6417538a9ec7f6b1a7bc7cfa74
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-12-09 21:56:05 +0100

    Update nouvelle_trajectoire.c

--- sources/nouvelle_trajectoire.c
+++ sources/nouvelle_trajectoire.c
@@ -6,6 +6,8 @@
 struct trajectoire *nouvelle_trajectoire() {
     struct trajectoire *trajectoire = (struct trajectoire *) malloc(sizeof(struct trajectoire));
     
+    int choix_systeme;
+    
     printf("quel systeme dynamique voulez-vous ? Lorentz, Hugo ou Mihaja ? \n");
     printf("Lorenz : \n dx/dt=σ(y − x) \n dy/dt=x(ρ − z) − y \n dz/dt=xy − βz \n tapez 1 \n");
     printf("hugo : \n dx/dt=σ(y + x) \n dy/dt=ρ(x − z) − z \n dz/dt=x − z \n tapez 2 \n");
