91b478ef91d9b280875de3a03c2b07dd93aa8ac7
Rmihaja <50056520+Rmihaja@users.noreply.github.com>
2021-12-10 20:47:08 +0100

    🚩 Add automatic opening of gnuplot from program
    Add gnuplot pipe to pass commands within program
    Add ui decoration

--- sources/main.c
+++ sources/main.c
@@ -1,8 +1,22 @@
 #include "trajectoire.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main()
 {
+    printf("\n***************************\n");
+    printf("* Bienvenue dans Enigma ! *");
+    printf("\n***************************\n");
+
     struct trajectoire *lorenz = nouvelle_trajectoire();
     calcul_trajectoire(lorenz);
+
+    printf("\nCourbe tracée avec succès !\n");
+    printf("Ouverture de gnuplot...\n");
+    printf("(Veuillez vous assurez que gnuplot est bien installé sur le système)\n");
+    FILE *gnuplot_pipe;
+    gnuplot_pipe = popen("gnuplot -p","w");
+    fprintf(gnuplot_pipe, "set parametric\n");
+    fprintf(gnuplot_pipe, "splot 'sortie.dat' u 2:3:4\n");
     return 0;
 }
