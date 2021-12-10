c2ee3ed70eff6ee510ea803dd9643794c2686c01
Le Padawan noir <90393808+Hugo-Avril@users.noreply.github.com>
2021-12-10 22:01:45 +0100

    Update nouvelle_position.c

--- sources/tools/nouvelle_position.c
+++ sources/tools/nouvelle_position.c
@@ -12,7 +12,12 @@ struct position *nouvelle_position(float x, float y, float z) {
 #ifdef TESTS
 
 int main() {
-    // tests here
+    struct position *position = (struct position *) malloc(sizeof(struct position));
+    position=nouvelle_position(1.0,2.0,3.0);
+    TEST_RES(position->x==1);
+    TEST_RES(position->y==2);
+    TEST_RES(position->x==3);
+    
     return 0;
 }
 
